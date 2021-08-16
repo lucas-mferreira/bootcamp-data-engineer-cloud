c69d7e2c4dcd6da017956d96d76c36d670d1c60c
Lucas Ferreira <lucasmarcelinof@gmail.com>
2021-08-16 20:30:37 -0300

    beckend tf s3

---
+++ infrastructure/s3.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "dl" {
+    bucket = "datalake-lucas-igti-tf"
+    acl = "private"
+
+
+    tags = {
+        IES = "IGTI",
+        CURSO = "EDC"
+    }
+
+    server_side_encryption_configuration{
+        rule {
+            apply_server_side_encryption_by_default {
+                see_algorithm = "AES256"
+            }
+        }
+    }
+}
