cfde8afa3aaca18cc0c91deeaa94b6cedce3960c
Lucas Ferreira <lucasmarcelinof@gmail.com>
2021-08-16 19:32:40 -0300

    frist commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "datalake" {
+  # Parâmetros de configuração do recurso escolhido
+  bucket = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
